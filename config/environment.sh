0bf07ff39ede24f7801dfee66b2ab3323a9074b8
gcielniak <gcielniak@lincoln.ac.uk>
2023-09-26 11:56:21 +0100

    Initial commit

---
+++ config/environment.sh
@@ -0,0 +1,18 @@
+# Field Details
+export FIELD_NAME='main'
+export FIELD_ID='0'
+export FIELD_TYPE='base'
+
+# Connection details
+export MQTT_IP='0.0.0.0'
+export MQTT_PORT='8883'
+export MQTT_NS=''
+
+# Config Quick-References
+#CONF=$(ros2 pkg prefix environment_template)/share/environment_template/config/
+#
+#export DATUM_FILE="$CONF/location/datum.yaml"
+#if [ ! -f "$FILE" ]; then export DATUM_FILE="$CONF/location/datum_autogen.yaml" ; fi
+#
+#export TMAP_FILE="$CONF/topological/network.tmap2.yaml"
+#if [ ! -f "$FILE" ]; then export TMAP_FILE="$CONF/topological/network_autogen.tmap2.yaml" ; fi
